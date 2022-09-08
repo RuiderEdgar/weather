23650d0db2a78bb8e279de3fecc44e9ddb6d9fc9
Edgar Rivera <edgar.riveraj@gmail.com>
2022-09-08 17:31:04 -0500

    design ready

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import './sass/main.scss';
+import './assets/snow.gif';
+import './assets/github.png';
--- webpack.config.js
+++ webpack.config.js
@@ -19,22 +19,22 @@ module.exports = {
                 test: /\.s[ac]ss$/i,
                 use: ["style-loader", "css-loader", "sass-loader", ]
             },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
             // {
             //     test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            //     use: [{
-            //         loader: "file-loader",
-            //         options: {
-            //             name: 'assets/[name].[ext]',
-            //             output: 'assets',
-            //             useRelativePath: true,
-            //             publicPath: 'assets',
-            //         }
-            //     }]
-            // }
+            //     type: 'asset/resource',
+            // },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        name: 'assets/[name].[ext]',
+                        output: 'assets',
+                        useRelativePath: true,
+                        publicPath: 'assets',
+                    }
+                }]
+            }
         ]
     },
     devServer: {
